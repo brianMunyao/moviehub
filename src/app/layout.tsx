7d8029fc0450abdb1b93d1d6ebe5adbe96fd4a31
brianMunyao <brianmunyao6@gmail.com>
2025-09-01 04:15:53 +0300

    feat: implement footer component for layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/global/providers";
+import Footer from "@/components/global/footer";
 
 const montserratSans = Montserrat({
   variable: "--font-montserrat-sans",
@@ -21,7 +22,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Providers>
-        <body className={`${montserratSans.className} antialiased`}>{children}</body>
+        <body className={`${montserratSans.className} antialiased`}>
+          {children}
+
+          <Footer />
+        </body>
       </Providers>
     </html>
   );
